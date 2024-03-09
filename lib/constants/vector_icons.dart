c47186f1c0299e8085e9db0885e93f38a96d7bb8
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2024-03-10 00:21:05 +0800

    refactor: change the class name to CustomIcons

--- lib/constants/vector_icons.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_svg/flutter_svg.dart';
-
-class VectorIcons{
-  static SvgPicture facebook = SvgPicture.asset("icons/Icon-Facebook.svg");
-  static SvgPicture google = SvgPicture.asset("icons/Icon-Google.svg");
-  static SvgPicture x = SvgPicture.asset("icons/Icon-X.svg");
-}
---
+++ lib/utils/custom_icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomIcons{
+  static Widget email = SvgPicture.asset("icons/Icon-Email.svg");
+  static Widget password = SvgPicture.asset("icons/Icon-Password.svg");
+  static Widget mini_calendar = SvgPicture.asset("icons/Icon-Password.svg");
+}
