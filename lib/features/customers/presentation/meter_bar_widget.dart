f2dbcf1ad8fdc417ae24794d3e9b4d31d6e36216
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2024-06-02 01:02:34 +0800

    feat: add meter bar as widget

---
+++ lib/features/customers/presentation/meter_bar_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:loyalty_app/utils/custom_icons.dart';
+
+class MeterBar extends StatefulWidget {
+  const MeterBar({super.key});
+
+  @override
+  State<MeterBar> createState() => _MeterBarState();
+}
+
+class _MeterBarState extends State<MeterBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Stack(
+        fit: StackFit.loose,
+        alignment: Alignment.centerLeft,
+        children: [
+          Material(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(20),
+            elevation: 5,
+            child: LinearProgressIndicator(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xFF6590FF),
+              backgroundColor: const Color(0xFFD0DDFF),
+              value: 0.3,
+              minHeight: 30,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2.0),
+            child: CustomIcons.meter_bar_icon,
+          ),
+        ],
+      ),
+    );
+  }
+}
