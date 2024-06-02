25c859d7403f79a960c61011ef6bbfc6faadc79f
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2024-06-03 02:55:00 +0800

    feat: add card flip behavior to the card widget

--- lib/features/customers/presentation/home_screen.dart
+++ lib/features/customers/presentation/home_screen.dart
@@ -1,4 +1,12 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_flip_card/flutter_flip_card.dart';
+import 'package:loyalty_app/common_widgets/bottom_appbar_widget.dart';
+import 'package:loyalty_app/common_widgets/top_appbar_widget.dart';
+import 'package:loyalty_app/features/customers/presentation/meter_bar_widget.dart';
+import 'package:loyalty_app/utils/cards.dart';
+import 'card_tiers_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,14 +16,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  User user = FirebaseAuth.instance.currentUser!;
+  final flipController = FlipCardController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xFFF9F9F9),
-      body: Container(
-        color: Colors.transparent,
-        // color: Colors.yellow,
+      extendBody: true,
+      extendBodyBehindAppBar: true,
+      appBar: const TopAppBar(height: 105),
+      body: SizedBox(
         width: double.infinity,
         child: SingleChildScrollView(
           child: Column(
@@ -23,11 +35,9 @@ class _HomeScreenState extends State<HomeScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const SizedBox(height: 135),
-              Container(
-                color: Colors.transparent,
-                // color: Colors.blue,
+              const SizedBox(
                 width: double.infinity,
-                child: const Padding(
+                child: Padding(
                   padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
                   child: Text(
                     "HOME",
@@ -39,136 +49,77 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
-              Container(
-                color: Colors.transparent,
-                // color: Colors.red,
+              SizedBox(
                 width: double.infinity,
                 height: 235,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                  child: Material(
-                    color: Colors.white,
-                    // color: Colors.green,
-                    elevation: 5,
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topRight,
-                          end: Alignment.bottomLeft,
-                          colors: [
-                            Color(0xFF0A4400),
-                            Color(0xFF1B1B1B),
-                          ],
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
+                  child: GestureDetector(
+                    onTap: () {
+                      flipController.flipcard();
+                    },
+                    child: FlipCard(
+                      animationDuration: const Duration(milliseconds: 300),
+                      axis: FlipAxis.horizontal,
+                      frontWidget: CardTier(card: Cards.white, name: user.displayName),
+                      backWidget: const Placeholder(),
+                      controller: flipController,
+                      rotateSide: RotateSide.left,
+                      onTapFlipping: true,
                     ),
                   ),
                 ),
               ),
-              const SizedBox(height: 10),
-              Container(
-                color: Colors.transparent,
+              const SizedBox(
                 width: double.infinity,
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-                  child: SizedBox(
-                    height: 30,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Flexible(
-                          fit: FlexFit.loose,
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Discover",
-                              style: TextStyle(
-                                color: Color(0xFF4E4E4E),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Flexible(
+                            fit: FlexFit.loose,
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "LOYALTY METER:",
+                                style: TextStyle(
+                                  color: Color(0xFF4E4E4E),
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w900,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Flexible(
-                          fit: FlexFit.loose,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: InkWell(
-                              onTap: () {},
-                              child: const Padding(
+                          Flexible(
+                            fit: FlexFit.loose,
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Padding(
                                 padding: EdgeInsets.symmetric(horizontal: 1),
                                 child: Text(
-                                    '>',
+                                    'Earn {x} to maintain your status',
                                     style: TextStyle(
                                       height: 1,
-                                      color: Color(0xFF6590FF),
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.w600,
+                                      color: Color(0xFF4E4E4E),
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w400,
                                     ),
                                   ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
+                        ],
+                      ),
+                      MeterBar(),
+                    ],
                   ),
                 ),
               ),
-              Container(
-                color: Colors.transparent,
-                width: double.infinity,
-                height: 160,
-                child: const SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 15),
-                          child: Material(
-                            color: Colors.white,
-                            elevation: 5,
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            child: SizedBox(
-                              height: 160,
-                              width: 150,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(right: 15),
-                          child: Material(
-                            color: Colors.white,
-                            elevation: 5,
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            child: SizedBox(
-                              height: 150,
-                              width: 150,
-                            ),
-                          ),
-                        ),
-                        Material(
-                          color: Colors.white,
-                          elevation: 5,
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          child: SizedBox(
-                            height: 150,
-                            width: 150,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                color: Colors.transparent,
+              const SizedBox(height: 50),
+              SizedBox(
                 width: double.infinity,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
@@ -182,7 +133,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Align(
                             alignment: Alignment.centerLeft,
                             child: Text(
-                              "Earnings",
+                              "Badges",
                               style: TextStyle(
                                 color: Color(0xFF4E4E4E),
                                 fontSize: 20,
@@ -217,27 +168,156 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
-              Container(
-                color: Colors.red,
+              SizedBox(
                 width: double.infinity,
-                height: 410,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(20),
+                    color: const Color(0xFF6590FF),
+                    child: SizedBox(
+                      height: 135,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: Column(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "0 of 0 Badges",
+                                    style: TextStyle(
+                                      color: Color(0xFF4E4E4E),
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 80,
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: const Color(0x266590FF),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: const Color(0x266590FF),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: const Color(0x266590FF),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: const Color(0x266590FF),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(100),
+                                        color: const Color(0x266590FF),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                  child: SizedBox(
+                    height: 30,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Flexible(
+                          fit: FlexFit.loose,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Get More Points",
+                              style: TextStyle(
+                                color: Color(0xFF4E4E4E),
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          fit: FlexFit.loose,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: InkWell(
+                              onTap: () {},
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 1),
+                                child: Text(
+                                  'See all',
+                                  style: TextStyle(
+                                    height: 1,
+                                    color: Color(0xFF6590FF),
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                   child: Material(
-                    color: Colors.green,
                     elevation: 5,
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    child: SizedBox(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color(0xFFFFFFFF),
+                    child: const SizedBox(
                       height: 150,
                       width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 30),
-                        child: Container(
-                          height: 30,
-                          width: double.infinity,
-                          color: Colors.orange,
-                        ),
-                      ),
                     ),
                   ),
                 ),
@@ -247,6 +327,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      bottomNavigationBar: const CustomBottomAppBar(),
     );
   }
 }
