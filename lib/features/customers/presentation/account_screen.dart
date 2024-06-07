76de13602172d957519ee2d96911a2386988bd1e
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2024-06-08 04:53:51 +0800

    refactor: change to tappable item widget

--- lib/features/customers/presentation/account_screen.dart
+++ lib/features/customers/presentation/account_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:loyalty_app/features/authentication/authentication_service.dart';
+import 'package:loyalty_app/common_widgets/tappable_item_widget.dart';
+import 'package:loyalty_app/features/authentication/application/authentication_service.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
@@ -40,58 +41,29 @@ class _AccountScreenState extends State<AccountScreen> {
               ),
             ),
             const SizedBox(height: 20),
-            SizedBox(
-              height: 84,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    width: double.infinity,
-                    height: 35,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "PREFERENCE",
-                          style: TextStyle(
-                            color: Color(0xFF4E4E4E),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w900,
-                          ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  width: double.infinity,
+                  height: 35,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "PREFERENCE",
+                        style: TextStyle(
+                          color: Color(0xFF4E4E4E),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w900,
                         ),
                       ),
                     ),
                   ),
-                  Container(
-                    height: 46,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFFFFFFFF),
-                      border: Border(bottom: BorderSide(color: Color(0x80959595)))
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(width: 40, height: 40, child: Placeholder()),
-                          SizedBox(width: 10),
-                          Expanded(
-                            child: Text(
-                              "Dark Mode",
-                              style: TextStyle(
-                                color: Color(0xFF4E4E4E),
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+                TappableItem(text: "Dark Mode", onTap: () {}),
+              ],
             ),
             const SizedBox(height: 20),
             SizedBox(
@@ -99,7 +71,7 @@ class _AccountScreenState extends State<AccountScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const SizedBox(
+                  SizedBox(
                     width: double.infinity,
                     height: 35,
                     child: Padding(
@@ -119,113 +91,14 @@ class _AccountScreenState extends State<AccountScreen> {
                   ),
                   Column(
                     children: [
-                      Container(
-                        height: 46,
-                        decoration: const BoxDecoration(
-                            color: Color(0xFFFFFFFF),
-                            border: Border(bottom: BorderSide(color: Color(0x80959595)))
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 30),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(width: 40, height: 40, child: Placeholder()),
-                              SizedBox(width: 10),
-                              Expanded(
-                                child: Text(
-                                  "User Profile",
-                                  style: TextStyle(
-                                    color: Color(0xFF4E4E4E),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 46,
-                        decoration: const BoxDecoration(
-                            color: Color(0xFFFFFFFF),
-                            border: Border(bottom: BorderSide(color: Color(0x80959595)))
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 30),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(width: 40, height: 40, child: Placeholder()),
-                              SizedBox(width: 10),
-                              Expanded(
-                                child: Text(
-                                  "Security",
-                                  style: TextStyle(
-                                    color: Color(0xFF4E4E4E),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 46,
-                        decoration: const BoxDecoration(
-                            color: Color(0xFFFFFFFF),
-                            border: Border(bottom: BorderSide(color: Color(0x80959595)))
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 30),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(width: 40, height: 40, child: Placeholder()),
-                              SizedBox(width: 10),
-                              Expanded(
-                                child: Text(
-                                  "Link to Socials",
-                                  style: TextStyle(
-                                    color: Color(0xFF4E4E4E),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 46,
-                        decoration: const BoxDecoration(
-                            color: Color(0xFFFFEFEF),
-                            border: Border(bottom: BorderSide(color: Color(0x80959595)))
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 30),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(width: 40, height: 40, child: Placeholder()),
-                              SizedBox(width: 10),
-                              Expanded(
-                                child: Text(
-                                  "Authorized Access",
-                                  style: TextStyle(
-                                    color: Color(0xFFFF7373),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                      TappableItem(text: "User Profile", onTap: () {}),
+                      TappableItem(text: "Security", onTap: () {}),
+                      TappableItem(text: "Link to Socials", onTap: () {}),
+                      TappableItem(
+                        text: "Authorized Access",
+                        backgroundColor: Color(0xFFFFEFEF),
+                        textColor: Color(0xFFFF7373),
+                        onTap: () {},
                       ),
                     ],
                   ),
