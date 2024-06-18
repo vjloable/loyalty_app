512b7d52d2960199bd843fec25c297571156fd29
vjloable <vincejaphethloable@gmail.com>
2024-06-18 16:53:35 +0800

    feat: add the assets of dark mode, user profile, security, link to socials, and authorized access to widget icons

--- lib/utils/custom_icons.dart
+++ lib/utils/custom_icons.dart
@@ -6,11 +6,11 @@ class CustomIcons{
   static Widget password = SvgPicture.asset("icons/Icon-Password.svg");
   static Widget mini_calendar = SvgPicture.asset("icons/Icon-MiniCalendar.svg");
 
-  static Widget home = SvgPicture.asset("icons/Icon_Home.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
+  static Widget home = SvgPicture.asset("icons/Icon-Home.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
   static Widget wallet = SvgPicture.asset("icons/Icon-Wallet.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
   static Widget discover = SvgPicture.asset("icons/Icon-Discover.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
   static Widget profile = SvgPicture.asset("icons/Icon-Profile.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
-  static Widget home_active = SvgPicture.asset("icons/Icon_Home.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
+  static Widget home_active = SvgPicture.asset("icons/Icon-Home.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
   static Widget wallet_active = SvgPicture.asset("icons/Icon-Wallet.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
   static Widget discover_active = SvgPicture.asset("icons/Icon-Discover.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
   static Widget profile_active = SvgPicture.asset("icons/Icon-Profile.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
@@ -20,4 +20,14 @@ class CustomIcons{
   static Widget info = SvgPicture.asset("icons/Icon-Info.svg", colorFilter: const ColorFilter.mode(Color(0xFFFFFFFF), BlendMode.srcIn),);
   static Widget top_icon = SvgPicture.asset("icons/Icon-Loyalty-App-TopBar.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
   static Widget meter_bar_icon = SvgPicture.asset("icons/Icon-Loyalty-Meter-Bar.svg",);
+
+  static Widget dark_mode = SvgPicture.asset("icons/Icon-DarkMode.svg");
+  static Widget user_profile = SvgPicture.asset("icons/Icon-UserProfile.svg");
+  static Widget security = SvgPicture.asset("icons/Icon-Security.svg");
+  static Widget link_to_socials = SvgPicture.asset("icons/Icon-LinkToSocials.svg");
+  static Widget authorized_access = SvgPicture.asset("icons/Icon-AuthorizedAccess.svg");
+
+
+
+
 }
