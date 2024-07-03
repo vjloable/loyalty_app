ae06e1f18776ce96d6a6340827529deff8ff1e15
vjloable <vincejaphethloable@gmail.com>
2024-07-03 16:04:56 +0800

    feat: initialize a class wrapping widget with svg illustrations

---
+++ lib/utils/illustration_vectors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class IllustrationVectors {
+  static Widget authorized_access_hero = SvgPicture.asset("icons/Icon-AuthorizedAccessHero.svg", fit: BoxFit.fitWidth);
+}
