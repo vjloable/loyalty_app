1516b41b42fd4b224ae6588951272c3af15f0301
vjloable <vincejaphethloable@gmail.com>
2024-07-21 02:25:01 +0800

    feat: initialize worker repository screen

---
+++ lib/features/worker/data/worker_repository.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../domain/worker_model.dart';
+
+
+final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+class WorkerRepository {
+  static Worker initialize(String name, String uid) {
+    Worker worker = Worker(name: name, uid: uid);
+    _firebaseFirestore
+        .collection("workers")
+        .withConverter(
+      fromFirestore: Worker.fromFirestore,
+      toFirestore: (Worker worker, options) => worker.toFirestore(),
+    )
+        .doc(uid)
+        .set(worker, SetOptions(merge: true))
+        .then((_) {
+      print("Successfully added worker");
+    }).onError((error, stackTrace) {
+      print("Error adding worker");
+    });
+    return worker;
+  }
+
+  static Future<Worker?> getWorkerDoc(String uid) async {
+    DocumentReference<Worker> userWorkerDocRef = _firebaseFirestore
+        .collection("workers")
+        .doc(uid)
+        .withConverter(
+      fromFirestore: Worker.fromFirestore,
+      toFirestore: (Worker worker, _) => worker.toFirestore(),
+    );
+    Worker? worker = (await userWorkerDocRef.get()).data();
+    return worker;
+  }
+
+  // static Future<UserModel?> getUserDoc(User user) async {
+  //   DocumentReference<UserModel> userModelDocRef = _firebaseFirestore
+  //       .collection("users")
+  //       .doc(user.uid)
+  //       .withConverter(
+  //     fromFirestore: UserModel.fromFirestore,
+  //     toFirestore: (UserModel userModel, _) => userModel.toFirestore(),
+  //   );
+  //   UserModel? userModel = (await userModelDocRef.get()).data();
+  //   return userModel;
+  // }
+}
