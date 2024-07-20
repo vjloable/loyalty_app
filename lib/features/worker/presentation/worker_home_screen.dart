0010080b0cd4330345a904fc8077b4cdb8122086
vjloable <vincejaphethloable@gmail.com>
2024-07-21 02:24:11 +0800

    feat: initialize worker home screen with basic scanning and gifting button actions

---
+++ lib/features/worker/presentation/worker_home_screen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flip_card/flutter_flip_card.dart';
+import 'package:loyalty_app/common_widgets/rounded_elevated_button.dart';
+import 'package:loyalty_app/features/worker/presentation/scanner/scan_capture_screen.dart';
+
+import '../domain/worker_model.dart';
+
+class WorkerHomeScreen extends StatefulWidget {
+  final Worker worker;
+  final Future<void> Function() callback;
+  const WorkerHomeScreen({super.key, required this.worker, required this.callback,});
+
+  @override
+  State<WorkerHomeScreen> createState() => _WorkerHomeScreenState();
+}
+
+class _WorkerHomeScreenState extends State<WorkerHomeScreen> {
+  final flipController = FlipCardController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: RefreshIndicator(
+        onRefresh: widget.callback,
+        displacement: 40,
+        edgeOffset: 40,
+        color: const Color(0xFF6590FF),
+        child: SingleChildScrollView(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 135),
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          "WORKER",
+                          style: TextStyle(
+                            color: Color(0xFF4E4E4E),
+                            fontSize: 24,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          "( ${widget.worker.branch??"Unassigned"} )",
+                          style: const TextStyle(
+                            color: Color(0xFF4E4E4E),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      widget.worker!.name??"",
+                      style: const TextStyle(
+                        color: Color(0xFF4E4E4E),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            const SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                child: SizedBox(
+                  height: 30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        fit: FlexFit.loose,
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Actions",
+                            style: TextStyle(
+                              color: Color(0xFF4E4E4E),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: RoundedElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ScanScreen(),));
+                      },
+                      text: "SCAN",
+                      backgroundColor: const Color(0xFFFFFFFF),
+                    ),
+                  ),
+                  const SizedBox(width: 10,),
+                  Expanded(
+                    child: RoundedElevatedButton(
+                      onPressed: () {},
+                      text: "GIFT",
+                      backgroundColor: const Color(0xFFFFFFFF),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
