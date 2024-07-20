4d09a225e13535654a08d0ccbfb0842cf131a226
vjloable <vincejaphethloable@gmail.com>
2024-07-21 02:21:39 +0800

    refactor: change the parameter to accept background color customization of the button

--- lib/common_widgets/rounded_elevated_button.dart
+++ lib/common_widgets/rounded_elevated_button.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class RoundedElevatedButton extends StatefulWidget {
   final String text;
   final Function()? onPressed;
-  const RoundedElevatedButton({super.key, this.text = "", this.onPressed});
+  final Color backgroundColor;
+  const RoundedElevatedButton({super.key, this.text = "", this.onPressed, required this.backgroundColor});
 
   @override
   State<RoundedElevatedButton> createState() => _RoundedElevatedButtonState();
@@ -12,32 +13,29 @@ class RoundedElevatedButton extends StatefulWidget {
 class _RoundedElevatedButtonState extends State<RoundedElevatedButton> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.08),
-              spreadRadius: 0,
-              blurRadius: 4,
-              offset: const Offset(0, 4),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            spreadRadius: 0,
+            blurRadius: 4,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: ElevatedButton(
+          onPressed: widget.onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: widget.backgroundColor,
+            minimumSize: const Size(double.infinity, 70),
+            surfaceTintColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-          ],
-        ),
-        child: ElevatedButton(
-            onPressed: widget.onPressed,
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFFFFFFFF),
-              minimumSize: const Size(double.infinity, 70),
-              surfaceTintColor: Colors.transparent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-            child: Text(widget.text, style: const TextStyle(color: Color(0xFF515151), fontSize: 14, fontWeight: FontWeight.w700))
-        ),
+          ),
+          child: Text(widget.text, style: const TextStyle(color: Color(0xFF515151), fontSize: 14, fontWeight: FontWeight.w700))
       ),
     );
   }
