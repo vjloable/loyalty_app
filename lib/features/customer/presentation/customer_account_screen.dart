2fc77deaa17b477cf64725b77c99d64c0eb4daca
vjloable <vincejaphethloable@gmail.com>
2024-07-29 19:41:47 +0800

    feat: add initialization of the flip switch state depending on the theme mode

--- lib/features/customer/presentation/customer_account_screen.dart
+++ lib/features/customer/presentation/customer_account_screen.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:loyalty_app/common_widgets/flip_switch_widget.dart';
 import 'package:loyalty_app/features/authentication/data/user_repository.dart';
 import 'package:loyalty_app/features/authentication/domain/user_model.dart';
+import 'package:loyalty_app/utils/state_handlers.dart';
 
 import '../../../common_widgets/tappable_item_widget.dart';
+import '../../../main.dart';
 import '../../../utils/custom_icons.dart';
 import '../../authentication/application/authentication_service.dart';
 import '../domain/customer_model.dart';
@@ -19,16 +22,23 @@ class CustomerAccountScreen extends StatefulWidget {
 
 class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
   User user = FirebaseAuth.instance.currentUser!;
+  FlipSwitchStateHandler flipSwitchStateHandler = FlipSwitchStateHandler();
+
+  @override
+  void initState() {
+    flipSwitchStateHandler.setFlipState(MyApp.themeNotifier.value == ThemeMode.dark);
+    super.initState();
+  }
 
   Widget _showIfAuthorized(AsyncSnapshot<GenericUser?> snapshot){
     if (snapshot.hasData) {
       GenericUser userModel = snapshot.data!;
       return userModel.permissionMax > 0 ? TappableItem(
-        icon: CustomIcons.authorized_access,
+        icon: CustomIcons.authorized_access(Theme.of(context).colorScheme.error),
         text: "Authorized Access",
-        backgroundColor: const Color(0xFFFFEFEF),
-        tappedColor: const Color(0xFFFF7373).withOpacity(0.3),
-        textColor: const Color(0xFFFF7373),
+        backgroundColor: Theme.of(context).colorScheme.onError,
+        tappedColor: Theme.of(context).colorScheme.error.withOpacity(0.3),
+        textColor: Theme.of(context).colorScheme.error,
         onTap: () {
           Navigator.of(context).push(
             PageRouteBuilder(
@@ -72,9 +82,9 @@ class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
             SizedBox(
               height: 30,
               child: Text(
-                widget.customer.name??"",
-                style: const TextStyle(
-                  color: Color(0xFF4E4E4E),
+                widget.customer.name ?? "",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onSecondary,
                   fontSize: 20,
                   fontWeight: FontWeight.w300,
                 ),
@@ -84,17 +94,17 @@ class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
             Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                const SizedBox(
+                SizedBox(
                   width: double.infinity,
                   height: 35,
                   child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
                     child: Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
                         "PREFERENCE",
                         style: TextStyle(
-                          color: Color(0xFF4E4E4E),
+                          color: Theme.of(context).colorScheme.onSecondary,
                           fontSize: 16,
                           fontWeight: FontWeight.w900,
                         ),
@@ -102,7 +112,19 @@ class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
                     ),
                   ),
                 ),
-                TappableItem(icon: CustomIcons.dark_mode, text: "Dark Mode", onTap: () {}),
+                TappableItem(
+                  icon: CustomIcons.dark_mode(Theme.of(context).colorScheme.onSecondary),
+                  backgroundColor: Theme.of(context).colorScheme.surface,
+                  text: "Dark Mode",
+                  onTap: null,
+                  child: FlipSwitch(
+                    flipSwitchStateHandler: flipSwitchStateHandler,
+                    onTap: () {
+                      flipSwitchStateHandler.flipTheSwitch();
+                      MyApp.themeNotifier.value = flipSwitchStateHandler.getFlipState ? ThemeMode.dark : ThemeMode.light;
+                    },
+                  ),
+                ),
               ],
             ),
             const SizedBox(height: 20),
@@ -111,17 +133,17 @@ class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const SizedBox(
+                  SizedBox(
                     width: double.infinity,
                     height: 35,
                     child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
                       child: Align(
                         alignment: Alignment.centerLeft,
                         child: Text(
                           "ACCOUNT",
                           style: TextStyle(
-                            color: Color(0xFF4E4E4E),
+                            color: Theme.of(context).colorScheme.onSecondary,
                             fontSize: 16,
                             fontWeight: FontWeight.w900,
                           ),
@@ -131,9 +153,24 @@ class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
                   ),
                   Column(
                     children: [
-                      TappableItem(icon: CustomIcons.user_profile, text: "User Profile", onTap: () {}),
-                      TappableItem(icon: CustomIcons.security, text: "Security", onTap: () {}),
-                      TappableItem(icon: CustomIcons.link_to_socials, text: "Link to Socials", onTap: () {}),
+                      TappableItem(
+                        icon: CustomIcons.user_profile(Theme.of(context).colorScheme.onSecondary),
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        text: "User Profile",
+                        onTap: () {},
+                      ),
+                      TappableItem(
+                        icon: CustomIcons.security(Theme.of(context).colorScheme.onSecondary),
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        text: "Security",
+                        onTap: () {},
+                      ),
+                      TappableItem(
+                        icon: CustomIcons.link_to_socials(Theme.of(context).colorScheme.onSecondary),
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        text: "Link to Socials",
+                        onTap: () {},
+                      ),
                       FutureBuilder(
                         future: UserRepository.getUserDoc(widget.customer.uid),
                         builder: (context, snapshot) {
@@ -154,10 +191,10 @@ class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
                 AuthenticationService.signOut();
                 Navigator.pop(context);
               },
-              child: const Text(
+              child: Text(
                 "LOGOUT",
                 style: TextStyle(
-                  color: Color(0xFF6590FF),
+                  color: Theme.of(context).colorScheme.primary,
                   fontSize: 16,
                   fontWeight: FontWeight.w900,
                 ),
@@ -167,5 +204,11 @@ class _CustomerAccountScreenState extends State<CustomerAccountScreen> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    flipSwitchStateHandler.dispose();
+    super.dispose();
   }
 }
