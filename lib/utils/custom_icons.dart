e2ce206c32dffc409f44fb53580ff14eccdfc70b
vjloable <vincejaphethloable@gmail.com>
2024-07-29 19:25:15 +0800

    feat: add color parameters to all icons and turn those into functions

--- lib/utils/custom_icons.dart
+++ lib/utils/custom_icons.dart
@@ -6,14 +6,10 @@ class CustomIcons{
   static Widget password = SvgPicture.asset("icons/Icon-Password.svg");
   static Widget mini_calendar = SvgPicture.asset("icons/Icon-MiniCalendar.svg");
 
-  static Widget home = SvgPicture.asset("icons/Icon-Home.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
-  static Widget wallet = SvgPicture.asset("icons/Icon-Wallet.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
-  static Widget discover = SvgPicture.asset("icons/Icon-Discover.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
-  static Widget profile = SvgPicture.asset("icons/Icon-Profile.svg", colorFilter: const ColorFilter.mode(Color(0xFF959595), BlendMode.srcIn));
-  static Widget home_active = SvgPicture.asset("icons/Icon-Home.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
-  static Widget wallet_active = SvgPicture.asset("icons/Icon-Wallet.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
-  static Widget discover_active = SvgPicture.asset("icons/Icon-Discover.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
-  static Widget profile_active = SvgPicture.asset("icons/Icon-Profile.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
+  static Widget home(Color color) => SvgPicture.asset("icons/Icon-Home.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget wallet(Color color) =>  SvgPicture.asset("icons/Icon-Wallet.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget discover(Color color) =>  SvgPicture.asset("icons/Icon-Discover.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget profile(Color color) =>  SvgPicture.asset("icons/Icon-Profile.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
 
   static Widget user_light = SvgPicture.asset("icons/Icon-DefaultUser.svg", colorFilter: const ColorFilter.mode(Color(0xFFFFFFFF), BlendMode.srcIn),);
   static Widget user_dark = SvgPicture.asset("icons/Icon-DefaultUser.svg", colorFilter: const ColorFilter.mode(Color(0xFF3D3D3D), BlendMode.srcIn),);
@@ -21,17 +17,16 @@ class CustomIcons{
   static Widget top_icon = SvgPicture.asset("icons/Icon-Loyalty-App-TopBar.svg", colorFilter: const ColorFilter.mode(Color(0xFF6590FF), BlendMode.srcIn),);
   static Widget meter_bar_icon = SvgPicture.asset("icons/Icon-Loyalty-Meter-Bar.svg",);
 
-  static Widget dark_mode = SvgPicture.asset("icons/Icon-DarkMode.svg");
-  static Widget user_profile = SvgPicture.asset("icons/Icon-UserProfile.svg");
-  static Widget security = SvgPicture.asset("icons/Icon-Security.svg");
-  static Widget link_to_socials = SvgPicture.asset("icons/Icon-LinkToSocials.svg");
-  static Widget authorized_access = SvgPicture.asset("icons/Icon-AuthorizedAccess.svg");
+  static Widget dark_mode(Color color) =>  SvgPicture.asset("icons/Icon-DarkMode.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget user_profile(Color color) =>  SvgPicture.asset("icons/Icon-UserProfile.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget security(Color color) =>  SvgPicture.asset("icons/Icon-Security.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget link_to_socials(Color color) =>  SvgPicture.asset("icons/Icon-LinkToSocials.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget authorized_access(Color color) =>  SvgPicture.asset("icons/Icon-AuthorizedAccess.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
 
-  static Widget back_arrow_light = SvgPicture.asset("icons/Icon-BackArrow.svg", colorFilter: const ColorFilter.mode(Color(0xFFFFFFFF), BlendMode.srcIn),);
-  static Widget back_arrow_dark = SvgPicture.asset("icons/Icon-BackArrow.svg", colorFilter: const ColorFilter.mode(Color(0xFF3D3D3D), BlendMode.srcIn),);
+  static Widget back_arrow(Color color) => SvgPicture.asset("icons/Icon-BackArrow.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn),);
 
-  static Widget mini_location = SvgPicture.asset("icons/deal_card/Icon-Location.svg");
-  static Widget mini_time = SvgPicture.asset("icons/deal_card/Icon-Time.svg");
-  static Widget mini_date = SvgPicture.asset("icons/deal_card/Icon-Date.svg");
-  static Widget mini_check = SvgPicture.asset("icons/deal_card/Icon-Check.svg");
+  static Widget mini_location(Color color) =>  SvgPicture.asset("icons/deal_card/Icon-Location.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget mini_time(Color color) =>  SvgPicture.asset("icons/deal_card/Icon-Time.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget mini_date(Color color) =>  SvgPicture.asset("icons/deal_card/Icon-Date.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
+  static Widget mini_check(Color color) =>  SvgPicture.asset("icons/deal_card/Icon-Check.svg", colorFilter: ColorFilter.mode(color, BlendMode.srcIn));
 }
