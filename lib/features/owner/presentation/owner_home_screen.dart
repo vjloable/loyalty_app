3f58e074e60a7e3d0e1a6e609f0b50115fb70d80
vjloable <vincejaphethloable@gmail.com>
2024-08-06 02:15:06 +0800

    refactor: add a button that goes to manager list screen

--- lib/features/owner/presentation/owner_home_screen.dart
+++ lib/features/owner/presentation/owner_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flip_card/controllers/flip_card_controllers.dart';
-import 'package:loyalty_app/features/owner/presentation/stores/store_add_screen.dart';
+import 'package:loyalty_app/features/owner/presentation/managers/manager_list_screen.dart';
+import 'package:loyalty_app/features/owner/presentation/stores/store_list_screen.dart';
 
 import '../../../common_widgets/rounded_elevated_button.dart';
 import '../../worker/presentation/scanner/scan_capture_screen.dart';
@@ -29,149 +30,165 @@ class _OwnerHomeScreenState extends State<OwnerHomeScreen> {
         color: const Color(0xFF6590FF),
         child: SingleChildScrollView(
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 135),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "OWNER",
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.onSecondary,
-                            fontSize: 24,
-                            fontWeight: FontWeight.w900,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 135),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "OWNER",
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w900,
+                            ),
                           ),
+                          const SizedBox(width: 8),
+                          Text(
+                            "( ${widget.owner.branches??"Unassigned"} )",
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        widget.owner.name??"",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSecondary,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300,
                         ),
-                        const SizedBox(width: 8),
-                        Text(
-                          "( ${widget.owner.branches??"Unassigned"} )",
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.onSecondary,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                  child: SizedBox(
+                    height: 30,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          fit: FlexFit.loose,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Actions",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onSecondary,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
                           ),
                         ),
                       ],
                     ),
-                    Text(
-                      widget.owner.name??"",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onSecondary,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: RoundedElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ScanScreen(),));
+                        },
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        child: Text("SCAN", style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 14, fontWeight: FontWeight.w700)),
+                      ),
+                    ),
+                    const SizedBox(width: 10,),
+                    Expanded(
+                      child: RoundedElevatedButton(
+                        onPressed: () {},
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        child: Text("GIFT", style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 14, fontWeight: FontWeight.w700)),
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-            const SizedBox(height: 50),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-                child: SizedBox(
-                  height: 30,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        fit: FlexFit.loose,
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Actions",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.onSecondary,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                  child: SizedBox(
+                    height: 30,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          fit: FlexFit.loose,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Manage",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onSecondary,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: RoundedElevatedButton(
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  children: [
+                    RoundedElevatedButton(
                       onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ScanScreen(),));
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => StoreListScreen(owner: widget.owner,),));
                       },
                       backgroundColor: Theme.of(context).colorScheme.surface,
-                      child: Text("SCAN", style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 14, fontWeight: FontWeight.w700)),
+                      child: Text("STORES", style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 14, fontWeight: FontWeight.w700)),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Expanded(
-                    child: RoundedElevatedButton(
-                      onPressed: () {},
-                      backgroundColor: Theme.of(context).colorScheme.surface,
-                      child: Text("GIFT", style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 14, fontWeight: FontWeight.w700)),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
-                child: SizedBox(
-                  height: 30,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        fit: FlexFit.loose,
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Manage",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.onSecondary,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Column(
-                children: [
-                  RoundedElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => StoreAddScreen(owner: widget.owner,),));
-                    },
-                    backgroundColor: Theme.of(context).colorScheme.surface,
-                    child: Text("STORES", style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 14, fontWeight: FontWeight.w700)),
-                  ),
-                ],
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  children: [
+                    RoundedElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ManagerListScreen(),));
+                      },
+                      backgroundColor: Theme.of(context).colorScheme.surface,
+                      child: Text("MANAGERS", style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontSize: 14, fontWeight: FontWeight.w700)),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ]),
+            ],
+          ),
         ),
       ),
     );
